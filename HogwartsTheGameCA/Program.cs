0738a92868d2ca1187133563f9a84336941fb128
EricTalv <eric.talviste@tptlive.ee>
2018-07-15 21:49:38 +0300

    Stuff added?

--- HogwartsTheGameCA/Program.cs
+++ HogwartsTheGameCA/Program.cs
@@ -1,15 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HogwartsTheGameCA
 {
-    class Program
+    public static class Game
     {
-        static void Main(string[] args)
+        static void Main()
         {
+
+            //Gameplay
+            Console.WriteLine("=============================================");
+            Console.WriteLine("Welcome!");
+            Console.WriteLine("Pick Solution");
+            Console.WriteLine("[ A ] - IF GAME\n [ B ] - IF GAME \n [ C ] - ARRAY GAME");
+            Console.WriteLine("=============================================");
+
+            string userInput = Console.ReadLine();
+            
+
         }
+
+        public static void IfGame()
+        {
+            Console.WriteLine("CHOOSE [A] [B]");
+            string userInput = Console.ReadLine();
+
+            if (userInput.ToLower() == "a")
+            {
+
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("You have chosen A! Awesome");
+                Console.ResetColor();
+            }
+
+            else if (userInput.ToLower() == "b")
+            {
+
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("You have chosen B! Beautiful");
+                Console.ResetColor();
+
+            }
+
+            else if (userInput.ToLower() == "q")
+            {
+                Environment.Exit(0);              
+            }
+
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("You have chosen Nothing!");
+                Console.ResetColor();
+            }
+        }
+
     }
 }
